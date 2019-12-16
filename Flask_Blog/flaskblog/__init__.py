c5730070c32ad2154c9bdb7e8378a064b583ee3b
Gordon Cheung <itsagordon@gmail.com>
2019-12-16 11:10:18 -0800

    Update __init__.py

--- Flask_Blog/flaskblog/__init__.py
+++ Flask_Blog/flaskblog/__init__.py
@@ -21,8 +21,6 @@ login_manager.login_message_category = 'info'
 #app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 #app.config['MAIL_PORT'] = 587
 #app.config['MAIL_USE_TLS'] = True
-#app.config['MAIL_USERNAME'] = 'itsgordonlol@gmail.com'#os.environ.get('EMAIL_USER')
-#app.config['MAIL_PASSWORD'] = 'UI0VQ8S7a!'#os.environ.get('EMAIL_PASS')
 #mail = Mail(app)
 mail = Mail()
 
